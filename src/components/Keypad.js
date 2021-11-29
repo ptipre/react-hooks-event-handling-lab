d72ee4a058ffb21c67256e3114bb4881481017ba
Prajay Tipre <prajaytipre@gmail.com>
2021-11-29 14:04:30 -0500

    commit tree fa9841eed9613bacbd2a50cdae64563dee9d1d5a

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+
+import React, { useState } from 'react';
+
+function Keypad() {
+
+    return (
+        <input type="password" onChange={(e) => console.log('Entering password...')}/>
+    )
+}
+
+export default Keypad;
